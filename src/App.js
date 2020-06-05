f5df312ab79caf566775ad0229952ffa0e1588e0
tim wu <timwuuk@gmail.com>
2020-06-05 22:17:48 +0100

    add multiple contexts and use context consumer

--- src/App.js
+++ src/App.js
@@ -3,14 +3,17 @@ import Navbar from "./components/Navbar";
 import BookList from "./components/BookList";
 import ThemeContextProvider from "./contexts/ThemeContext";
 import ToggleTheme from "./components/ToggleTheme";
+import AuthContextProvider from "./contexts/AuthContext";
 
 function App() {
   return (
     <div className="App">
       <ThemeContextProvider>
-        <Navbar />
-        <BookList />
-        <ToggleTheme />
+        <AuthContextProvider>
+          <Navbar />
+          <BookList />
+          <ToggleTheme />
+        </AuthContextProvider>
       </ThemeContextProvider>
     </div>
   );
