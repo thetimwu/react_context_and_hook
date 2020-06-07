de49b06ce4a828e73baba94da1a0290d66a6e8ba
tim wu <timwuuk@gmail.com>
2020-06-07 17:06:44 +0100

    add save to local storage func

--- src/contexts/NewBookContext.js
+++ src/contexts/NewBookContext.js
@@ -1,10 +1,18 @@
-import React, { createContext, useReducer } from "react";
+import React, { createContext, useReducer, useEffect } from "react";
 import BookReducer from "../reducers/BookReducers";
 
 export const NewBookContext = createContext();
 
 const NewBookContextProvider = (props) => {
-  const [books, dispatch] = useReducer(BookReducer, []);
+  const [books, dispatch] = useReducer(BookReducer, [], () => {
+    return localStorage.getItem("books")
+      ? JSON.parse(localStorage.getItem("books"))
+      : [];
+  });
+
+  useEffect(() => {
+    localStorage.setItem("books", JSON.stringify(books));
+  }, [books]);
 
   return (
     <NewBookContext.Provider value={{ books, dispatch }}>
